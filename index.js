b21223ea79806db0d00c5591440d0ce8b78db12f
dorothymuhonja <dorothymuhonja7@gmail.com>
2022-06-17 18:15:41 +0300

    commit tree 0b05b11b5534c845012abc8f8a0ae404bbd5b150

--- index.js
+++ index.js
@@ -1,1 +1,34 @@
 // Code your solution in this file!
+function distanceFromHqInBlocks(distance){
+    return Math.abs(distance - 42)
+    
+}
+distanceFromHqInBlocks();
+
+
+function distanceFromHqInFeet(distance) {
+    return distanceFromHqInBlocks(distance) * 264
+
+}
+distanceFromHqInFeet()
+
+function distanceTravelledInFeet(start, destination){
+    return Math.abs((start - destination) * 264); 
+
+}
+distanceTravelledInFeet()
+
+function calculatesFarePrice(start, destination) {
+    const distanceTravelled = distanceTravelledInFeet(start, destination);
+
+    if (distanceTravelled < 400) {
+        return 0;
+    } else if (distanceTravelled > 400 && distanceTravelled <= 2000 ) {
+        return (distanceTravelled - 400 ) * 0.02;
+    } else if (distanceTravelled > 2000 && distanceTravelled < 2500) {
+        return 25;
+    } else {
+        return `cannot travel that far`
+    }
+
+}
